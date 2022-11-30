5c01bbab4cb2abd79bd1fca3bc90e4084b6ad89d
mxfs777 <117290945+mxfs777@users.noreply.github.com>
2022-11-30 01:14:23 +0100

    Added style of aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -3,12 +3,38 @@ import "./Aside.css";
 const Aside = () => {
     return ( 
         <aside className='aside'>
-            <ul>
-                <li>Profile</li>
-                <li>Admin Dashboard</li>
-                <li>Contacts</li>
-                <li>Oportunities</li>
-                <li>Customers</li>
+            <div className="avatarContainer">
+                <div>
+                    <img className="avatar" src="https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png" alt="avatar"  />
+                    <h3>Name</h3>
+                </div>
+            </div>
+            <ul className="menuItemsContainer">
+                <li>
+                    <div>
+                        <i class="fa-regular fa-user fa-2xl"></i>
+                        <p>Profile</p>
+                    </div>
+                </li>
+                {/* <li>Admin Dashboard</li> */}
+                <li>
+                    <div>
+                        <i class="fa-regular fa-address-book  fa-2xl"></i>
+                        <p>Contacts</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <i class="fa-regular fa-lightbulb fa-2xl"></i>
+                        <p>Oportunities</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <i class="fa-solid fa-briefcase fa-2xl"></i>
+                        <p>Customers</p>
+                    </div>
+                </li>
             </ul>
         </aside>
      );
